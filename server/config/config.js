97b0e1b16f418637474679c1ba1e9fdbc3d564c9
RamirezJosue <xdemobtkx@gmail.com>
2018-10-26 16:11:51 -0500

    rest de usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,19 @@
 // ========================
 
 process.env.PORT = process.env.PORT || 3000;
+
+// ========================
+// Entorno
+// ========================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+// ========================
+// Base de datos
+// ========================
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/asistencia';
+} else {
+    urlDB = 'mongodb://asistencia-user:d3velopers@ds231643.mlab.com:31643/asistencia';
+}
+
+process.env.URLDB = urlDB;
