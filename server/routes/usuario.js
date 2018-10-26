4bf17e8d988221e867a47eee2a01bfec4eee64cd
RamirezJosue <xdemobtkx@gmail.com>
2018-10-26 07:15:31 -0500

    PUT, GET, DELETE

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const _ = require('underscore');
 const Usuario = require('../models/usuario')
 
 const app = express();
 
 
 app.get('/usuario', function(req, res) {
-    res.json('get Usuario')
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+    Usuario.find({ estado: true }, 'user role estado')
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+            Usuario.count({ estado: true }, (err, conteo) => {
+                res.json({
+                    ok: true,
+                    usuarios,
+                    cuantos: conteo
+                });
+
+            })
+        });
 });
 
 app.post('/usuario', function(req, res) {
@@ -35,13 +58,48 @@ app.post('/usuario', function(req, res) {
 
 app.put('/usuario/:id', function(req, res) {
     let id = req.params.id;
-    res.json({
-        id
+    let body = _.pick(req.body, ['role', 'estado']);
+
+    Usuario.findByIdAndUpdate(id, body, { new: true }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        })
     })
 });
 
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
+app.delete('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    // Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+    let cambiaEstado = {
+        estado: false
+    }
+    Usuario.findByIdAndUpdate(id, cambiaEstado, (err, usuarioBorrado) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        if (!usuarioBorrado) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario no encontrado'
+                }
+            });
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioBorrado
+        })
+    })
 });
 
 module.exports = app;
