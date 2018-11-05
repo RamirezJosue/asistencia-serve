4ac7e919257e497a66286ef1f3dcc3fe03e7966d
RamirezJosue <merling.ramirez@upeu.edu.pe>
2018-11-05 12:37:46 -0500

    resolviendo conflictos

--- server/routes/login.js
+++ server/routes/login.js
@@ -7,46 +7,46 @@ const app = express();
 
 app.post('/login', (req, res) => {
 
-            let body = req.body;
-            Usuario.findOne({ user: body.user }).
-            populate('persona').
-            exec(function(err, usuarioDB) {
-                if (err) {
-                    return res.status(500).json({
-                        ok: false,
-                        err
-                    });
-                }
-                if (!usuarioDB) {
-                    return res.status(400).json({
-                        ok: false,
-                        err: {
-                            message: "Usuario o contraseña incorrectos"
-                        }
-                    });
-                }
-
-                if (!bcrypt.compareSync(body.clave, usuarioDB.clave)) {
-                    return res.status(400).json({
-                        ok: false,
-                        err: {
-                            message: "Usuario o contraseñas incorrectos"
-                        }
-                    });
-                }
-
-                let token = jwt.sign({
-                    usuario: usuarioDB
-                }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
-
-                res.json({
-                    ok: true,
-                    usuario: usuarioDB,
-                    id: usuarioDB._id,
-                    token
+    let body = req.body;
+    Usuario.findOne({ user: body.user })
+        .populate('persona')
+        .exec(function(err, usuarioDB) {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
                 });
+            }
+            if (!usuarioDB) {
+                return res.status(400).json({
+                    ok: false,
+                    err: {
+                        message: "Usuario o contraseña incorrectos"
+                    }
+                });
+            }
 
+            if (!bcrypt.compareSync(body.clave, usuarioDB.clave)) {
+                return res.status(400).json({
+                    ok: false,
+                    err: {
+                        message: "Usuario o contraseñas incorrectos"
+                    }
+                });
+            }
+
+            let token = jwt.sign({
+                usuario: usuarioDB
+            }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+
+            res.json({
+                ok: true,
+                Usuario: usuarioDB,
+                token
             });
+        });
+
+});
 
 
-            module.exports = app;
+module.exports = app;
